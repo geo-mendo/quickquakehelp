63cf70d8ff3094895f3b1f6f4f9b9a6a59c47c26
geo-mendo <geoffrey@mendo.cloud>
2023-09-11 12:55:02 +0100

    change name

--- src/presentation/views/Home/Home.tsx
+++ src/presentation/views/Home/Home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
     const navigate = useNavigate();
   return (
     <div className='flex flex-col h-screen justify-center items-center gap-4'>
-        <Typography type='h1' as="h1" className='text-3xl font-bold ' ><span className='text-red-900' >Quick Quake</span> <span className='text-light-green-900' >Help</span></Typography>
+        <Typography type='h1' as="h1" className='text-3xl font-bold text-teal-500' >Aide mon <span className='text-red-900' >vi</span><span className='text-light-green-900' >lla</span><span className='text-red-900' >ge</span></Typography>
         <img src={moroccoFlag} alt='morocco flag' width={250}/>
         <div className='flex flex-col justify-center items-center gap-4'>
             <Button fullWidth color='teal' onClick={() => navigate(ROUTES.NEED_LIST)} >Voir les besoins d'aide !</Button>
